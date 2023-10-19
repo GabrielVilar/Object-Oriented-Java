1747cd03cb16de6f3a66467a8c2dedab20b65833
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-10-18 22:08:31 -0300

    Clothe

---
+++ Exercises/TravelOrganizer/src/Clothe.java
@@ -0,0 +1,28 @@
+public class Clothe extends Luggage_Item{
+    
+    private double min_temp;
+    private boolean water_resistence;
+    
+    public Clothe(String name, String description, String picture, double min_temp, boolean water_resistence) {
+        super(name, description, picture);
+        this.min_temp = min_temp;
+        this.water_resistence = water_resistence;
+    }
+
+    public void setMin_temp(double min_temp) {
+        this.min_temp = min_temp;
+    }
+
+    public void setWater_resistence(boolean water_resistence) {
+        this.water_resistence = water_resistence;
+    }
+
+    @Override
+    public String toString() {
+        return "Clothe [min_temp=" + min_temp + ", water_resistence=" + water_resistence + "]";
+    }
+
+    public void cadastra_roupa(){
+    
+    }   
+}
