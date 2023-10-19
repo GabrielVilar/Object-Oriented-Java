074def0b604369f4e9f8d4e445a3df802e46f3a9
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-10-18 21:50:01 -0300

    Accessoreis changes

--- Exercises/TravelOrganizer/src/Accessories.java
+++ Exercises/TravelOrganizer/src/Accessories.java
@@ -1,28 +1,25 @@
-public class Acessorio extends Item_de_bagagem{
+public class Accessories extends Item_de_bagagem{
 
-    private String tipo;
-    private String regiao_corpo;
+    private String type;
+    private String corporal_region;
 
-    public Acessorio(String nome, String descricao, String foto, String tipo, String regiao_corpo) {
+    public Accessories(String nome, String descricao, String foto, String type, String corporal_region) {
         super(nome, descricao, foto);
-        this.tipo = tipo;
-        this.regiao_corpo = regiao_corpo;
+        this.type = type;
+        this.corporal_region = corporal_region;
     }
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
+    public void setType(String type) {
+        this.type = type;
     }
 
-    public void setRegiao_corpo(String regiao_corpo) {
-        this.regiao_corpo = regiao_corpo;
+    public void setCorporal_region(String corporal_region) {
+        this.corporal_region = corporal_region;
     }
 
     @Override
     public String toString() {
-        return "Acessorio [tipo=" + tipo + ", regiao_corpo=" + regiao_corpo + "]";
+        return "Accessories [type=" + type + ", corporal_region=" + corporal_region + "]";
     }
-
-    
-
     
 }
