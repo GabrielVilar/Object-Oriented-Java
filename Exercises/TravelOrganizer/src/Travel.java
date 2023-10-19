99951f6808e3c658106fb443586a1d8c98d1f6c4
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-10-18 22:17:01 -0300

    Travel

---
+++ Exercises/TravelOrganizer/src/Travel.java
@@ -0,0 +1,52 @@
+public class Travel {
+    
+    private String city;
+    private String country;
+    private String arrive_date;
+    private String departure_date;
+    private double min_temp;
+    private double max_temp;
+
+    public Travel(String city, String country, String arrive_date, String departure_date, double min_temp,
+            double max_temp) {
+        this.city = city;
+        this.country = country;
+        this.arrive_date = arrive_date;
+        this.departure_date = departure_date;
+        this.min_temp = min_temp;
+        this.max_temp = max_temp;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setArrive_date(String arrive_date) {
+        this.arrive_date = arrive_date;
+    }
+
+    public void setDeparture_date(String departure_date) {
+        this.departure_date = departure_date;
+    }
+
+    public void setMin_temp(double min_temp) {
+        this.min_temp = min_temp;
+    }
+
+    public void setMax_temp(double max_temp) {
+        this.max_temp = max_temp;
+    }
+
+    @Override
+    public String toString() {
+        return "Travel [city=" + city + ", country=" + country + ", arrive_date=" + arrive_date + ", departure_date="
+                + departure_date + ", min_temp=" + min_temp + ", max_temp=" + max_temp + "]";
+    }
+
+    public void registerTravel(){}
+    public void editTravel(){}
+}
