f718cdb0a1f84e1f023bc4a16745124570b3fabe
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-10-26 22:57:07 -0300

    Still working on it

--- Exercises/KidsParties/src/Party.java
+++ Exercises/KidsParties/src/Party.java
@@ -3,8 +3,6 @@ public class Party {
     private String start_date;
     private String end_date;
     private float total_value_party;
-    private Client cliente = new Client();
-    private Theme theme = new Theme();
     
     public String getStart_date() {
         return start_date;
@@ -14,12 +12,6 @@ public class Party {
     }
     public float getTotal_value_party() {
         return total_value_party;
-    }
-    public Client getCliente() {
-        return cliente;
-    }
-    public Theme getTheme() {
-        return theme;
     }
 
     public void setStart_date(String start_date) {
@@ -31,24 +23,13 @@ public class Party {
     public void setTotal_value_party(float total_value_party) {
         this.total_value_party = total_value_party;
     }
-    public void setCliente(Client cliente) {
-        this.cliente = cliente;
-    }
-    public void setTheme(Theme theme) {
-        this.theme = theme;
-    }
 
     public Party(){}
-    
-    public Party(String start_date, String end_date, float total_value_party, Client cliente, Theme theme) {
+
+    public Party(String start_date, String end_date, float total_value_party) {
         this.start_date = start_date;
         this.end_date = end_date;
         this.total_value_party = total_value_party;
-        this.cliente = cliente;
-        this.theme = theme;
-    }
-
-    
-    
+    }    
     
 }
---
+++ Exercises/KidsParties/src/PartyControll.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class PartyControll {
+
+    Client cliente = new Client();
+    Theme theme = new Theme();
+    Party party = new Party();
+    Scanner sc = new Scanner(System.in);
+
+    public void register_party(){
+
+    }
+    
+    public static void main(String[] args) {
+
+        
+
+        
+    }
+}
