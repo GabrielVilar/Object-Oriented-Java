56b9633da58b4a0589e0ed5f524725a448371e16
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-11-02 17:33:13 -0300

    variáveis

--- Projects/Course Management/src/test/Main.java
+++ Projects/Course Management/src/test/Main.java
@@ -7,5 +7,16 @@ import courseManagement.*;
 import view.TelaListagem;
 
 public class Main {
-    
+
+    private static Dados d = new Dados();
+    private static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        
+    }
+
+
+
+
+
 }
