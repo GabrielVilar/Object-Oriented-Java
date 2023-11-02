5e2501fd80018c84677b7f16f3f836f6aba2f17a
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-11-02 17:11:38 -0300

    (get, set e toString)

--- Projects/Course Management/src/courseManagement/Matricula.java
+++ Projects/Course Management/src/courseManagement/Matricula.java
@@ -14,4 +14,41 @@ public class Matricula {
         this.turma = turma;
     }
 
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public double getValorPago() {
+        return valorPago;
+    }
+
+    public String getTurma() {
+        return turma;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public void setValorPago(double valorPago) {
+        this.valorPago = valorPago;
+    }
+
+    public void setTurma(String turma) {
+        this.turma = turma;
+    }
+
+    @Override
+    public String toString() {
+        return "Matricula [aluno=" + aluno + ", data=" + data + ", valorPago R$" + valorPago + ", turma=" + turma + "]";
+    }
+
 }
