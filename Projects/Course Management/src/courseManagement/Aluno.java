22a4e6c713688b8669d234c4d8086afb76a904dc
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-11-02 17:12:57 -0300

    Novas mudanças

--- Projects/Course Management/src/courseManagement/Aluno.java
+++ Projects/Course Management/src/courseManagement/Aluno.java
@@ -1,8 +1,14 @@
 package courseManagement;
 
-public class Aluno {
+public class Aluno extends Pessoa{
 
     private String dataNasc;
     private String endereco;
+
+    public Aluno(String nome, String numID, String cpf, String numTel, String dataNasc, String endereco) {
+        super(nome, numID, cpf, numTel);
+        this.dataNasc = dataNasc;
+        this.endereco = endereco;
+    }
     
 }
