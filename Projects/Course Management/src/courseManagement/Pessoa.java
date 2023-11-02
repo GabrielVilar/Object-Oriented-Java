b85a7566aedf93aff7143b93facdf1176a2d3c56
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-11-02 17:05:53 -0300

    Constructor

--- Projects/Course Management/src/courseManagement/Pessoa.java
+++ Projects/Course Management/src/courseManagement/Pessoa.java
@@ -7,4 +7,11 @@ public abstract class Pessoa {
     protected String cpf;
     protected String numTel;
 
+    public Pessoa(String nome, String numID, String cpf, String numTel) {
+        this.nome = nome;
+        this.numID = numID;
+        this.cpf = cpf;
+        this.numTel = numTel;
+    }
+
 }
