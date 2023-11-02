f1b8bf72516d157ecba2b51f49d8ac1c91d21eb4
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-11-02 17:08:51 -0300

    Novas mudanças

--- Projects/Course Management/src/courseManagement/Professor.java
+++ Projects/Course Management/src/courseManagement/Professor.java
@@ -1,7 +1,12 @@
 package courseManagement;
 
-public class Professor {
+public class Professor extends Pessoa{
     
     private double valorHoraAula;
+
+    public Professor(String nome, String numID, String cpf, String numTel, double valorHoraAula) {
+        super(nome, numID, cpf, numTel);
+        this.valorHoraAula = valorHoraAula;
+    }
     
 }
