be063e4146fe680faa0b8b51ab738491d61e17e7
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-11-02 17:22:44 -0300

    Variáveis

--- Projects/Course Management/src/data/Dados.java
+++ Projects/Course Management/src/data/Dados.java
@@ -1,5 +1,16 @@
 package data;
 
+import courseManagement.*;
+
 public class Dados {
     
+    private Aluno[] alunos;
+    private int nAlunos = 0;
+    private Professor[] professores;
+    private int nProfs = 0;
+    private Curso[] cursos;
+    private int nCursos = 0;
+    private Matricula[] matriculas;
+    private int nMatriculas = 0;
+
 }
