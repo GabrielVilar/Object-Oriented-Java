8a7ef541c6a01949a26c8c1125847a5708634553
Gabriel Vilarinho <me.gabrielVilar@gmail.com>
2023-12-20 21:31:36 -0300

    Creating the calculator graphical interface

--- GUIs/Calculator/src/calculator/Calculator.java
+++ GUIs/Calculator/src/calculator/Calculator.java
@@ -2,4 +2,73 @@ package calculator;
 
 public class Calculator {
     
+    private float number01;
+    private float number02;
+    private float add;
+    private float sub;
+    private float div;
+    private float mult;
+
+    public Calculator(float number01, float number02, float add, float sub, float div, float mult) {
+        this.number01 = number01;
+        this.number02 = number02;
+        this.add = add;
+        this.sub = sub;
+        this.div = div;
+        this.mult = mult;
+    }
+
+    public float addition(float number01, float number02){
+        return add = (number01 + number02);
+    }
+
+    public float subtraction(float number01, float number02){
+        return sub = (number01 - number02);
+    }
+
+    public float division(float number01, float number02){
+        return div = ((number01) / (number02));
+    }
+
+    public float multiplication(float number01, float number02){
+        return mult = ((number01) * (number02));
+    }
+
+    public float getNumber01() {
+        return number01;
+    }
+    public float getNumber02() {
+        return number02;
+    }
+    public float getAdd() {
+        return add;
+    }
+    public float getSub() {
+        return sub;
+    }
+    public float getDiv() {
+        return div;
+    }
+    public float getMult() {
+        return mult;
+    }
+    public void setNumber01(float number01) {
+        this.number01 = number01;
+    }
+    public void setNumber02(float number02) {
+        this.number02 = number02;
+    }
+    public void setAdd(float add) {
+        this.add = add;
+    }
+    public void setSub(float sub) {
+        this.sub = sub;
+    }
+    public void setDiv(float div) {
+        this.div = div;
+    }
+    public void setMult(float mult) {
+        this.mult = mult;
+    }    
+
 }
--- GUIs/Calculator/src/view/CalculatorView.java
+++ GUIs/Calculator/src/view/CalculatorView.java
@@ -1,5 +1,113 @@
 package view;
 
-public class CalculatorView {
-    
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import calculator.Calculator;
+
+public class CalculatorView extends JFrame {
+
+    private JTextField numberField1;
+    private JTextField numberField2;
+    private JButton addButton;
+    private JButton subButton;
+    private JButton divButton;
+    private JButton multButton;
+
+    private Calculator calculator;
+
+    public CalculatorView() {
+
+        setTitle("Simple Calculator");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3, 2));
+
+        numberField1 = new JTextField();
+        numberField2 = new JTextField();
+
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        divButton = new JButton("/");
+        multButton = new JButton("*");
+
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performOperation('+');
+            }
+        });
+
+        subButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performOperation('-');
+            }
+        });
+
+        divButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performOperation('/');
+            }
+        });
+
+        multButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                performOperation('*');
+            }
+        });
+
+        add(new JLabel("Number 1: "));
+        add(numberField1);
+        add(new JLabel("Number 2: "));
+        add(numberField2);
+        add(addButton);
+        add(subButton);
+        add(divButton);
+        add(multButton);
+
+        calculator = new Calculator(0, 0, 0, 0, 0, 0);
+
+        setVisible(true);
+    }
+
+    private void performOperation(char operation) {
+        try {
+            float num1 = Float.parseFloat(numberField1.getText());
+            float num2 = Float.parseFloat(numberField2.getText());
+
+            calculator.setNumber01(num1);
+            calculator.setNumber02(num2);
+
+            float result = 0;
+            switch (operation) {
+                case '+':
+                    result = calculator.addition(num1, num2);
+                    break;
+                case '-':
+                    result = calculator.subtraction(num1, num2);
+                    break;
+                case '/':
+                    result = calculator.division(num1, num2);
+                    break;
+                case '*':
+                    result = calculator.multiplication(num1, num2);
+                    break;
+            }
+
+            JOptionPane.showMessageDialog(this, "Result: " + result, "Calculation Result",
+                    JOptionPane.INFORMATION_MESSAGE);
+
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(this, "Invalid input. Please enter valid numbers.", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        } catch (ArithmeticException e) {
+            JOptionPane.showMessageDialog(this, "Error in calculation. Division by zero is not allowed.", "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
 }
